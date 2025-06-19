3ec0ba9251b6c1070f5b221debcdf31ebcbdcf15
Neo-Smit <mr.neosmit1997@gmail.com>
2025-06-19 18:09:54 +0500

    Обновление было измнено server.dart

--- bin/server.dart
+++ bin/server.dart
@@ -20,7 +20,7 @@ final allowedChatIds = {int.parse(goalChatId)}; // разрешённые чат
 
 /// Получение access_token через Service Account
 Future<String> getAccessToken() async {
-  final serviceJson = Platform.environment['SERVICE_ACCOUNT'];
+  final serviceJson = Platform.environment['Service_Account'];
   if (serviceJson == null) throw Exception('❌ SERVICE_ACCOUNT is not set');
 
   final credentials = ServiceAccountCredentials.fromJson(jsonDecode(serviceJson));
